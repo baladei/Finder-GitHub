bc13923a05bd64780ec25acf4cfe2ca8f516a9db
Fernando Baladei <f.baladei@gmail.com>
2023-01-18 06:15:08 -0300

    'finishing' the project

---
+++ src/components/Error.tsx
@@ -0,0 +1,7 @@
+export const Error = () => {
+  return (
+    <div>
+        <p>Usuário não encontrado</p>
+    </div>
+  );
+};
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,14 +1,24 @@
 import { UserProps } from '../types/user';
 import { Search } from '../components/Search';
+import { User } from '../components/User';
+import { Error } from '../components/Error';
 import { useState } from 'react';
 
 export const Home = () => {
   const [user, setUser] = useState <UserProps | null> (null);
+  const [error, setError] = useState(false);
 
   const loadUser = async  (userName: string) => {
+    setError(false);
+    setUser(null);
 
     const res = await fetch(`https://api.github.com/users/${userName}`);
     const data = await res.json();
+
+    if (res.status === 404) {
+      setError(true);
+      return;
+    }
 
     const {avatar_url, login, location, followers, following} = data;
 
@@ -26,7 +36,8 @@ export const Home = () => {
   return (
     <div>
       <Search loadUser={loadUser} />
-      {user && <p>{user.login}</p>}
+      {user && <User {...user} />}
+      {error && <Error />}
     </div>
   );
 };
